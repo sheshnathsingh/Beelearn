261657bdba933e466015917b3d36f68a50f8ceec
sheshnathsingh <singhsheshnath204@gmail.com>
2026-04-29 23:03:34 +0530

    Add credentials option to authService POST request

--- elearningplatform-main/src/services/authService.js
+++ elearningplatform-main/src/services/authService.js
@@ -28,6 +28,7 @@ export const authService = {
       headers: {
         'Content-Type': 'application/json',
       },
+      credentials: "include", 
       body: JSON.stringify({ email, password }),
     });
 
